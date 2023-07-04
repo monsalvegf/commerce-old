38af4d6a01c28a86249121ea137a38de1600544a
Fernando <monsalvegf@yahoo.com>
2023-07-04 21:14:38 +0200

    He creado los modelos y los he migrado

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,27 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=64)
+
+class Auctions(models.Model):
+    auction_name = models.CharField(max_length=64)
+    description = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    date_created = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Bids(models.Model):
+    auction = models.ForeignKey(Auctions, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bid = models.DecimalField(max_digits=12, decimal_places=2)
+    date_bid = models.DateTimeField(auto_now_add=True)
+
+class Comments(models.Model):
+    auction = models.ForeignKey(Auctions, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.DecimalField(max_digits=12, decimal_places=2)
+    date_comment = models.DateTimeField(auto_now_add=True)
